f66997bb6af667f75b044bbb04034111fc6d4e89
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-09-28 22:29:45 +1300

    update testing
    update the different collection

--- routes/viewable.js
+++ routes/viewable.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoConfig = require('../config/mongo-config');
 const db = mongoConfig.getDb();
 const submittedCollection = "SubmittedArticles";// need to change to viewable collection when added
+const rejectedCollection = "RejectedArticles";// need to change to viewable collection
+const acceptedCollection = "AcceptedArticles";// need to change to viewable collection
+const viewableCollection = "ViewableArticles";// need to change to viewable collection
 
 router.use(express.json());
 
@@ -40,6 +43,36 @@ router.get('/view/:title', async (req, res) => {
         //}
         } catch (error) {
             console.error(error);
+        res.status(500).send("Error connecting to database"); //cannot connect to database 
+    }
+})
+
+router.get('/accepted', async (req, res) => {
+    try {
+        let viewable = await db.collection(acceptedCollection).find({}, { article: {_id: req, title: req}}).toArray();
+        
+        if (viewable.length !== 0) {// displays all articles
+            res.status(200).json(viewable); 
+        } else {
+            res.status(404).send("articles list data not found"); //aricles not found
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error connecting to database"); //cannot connect to database 
+    }
+})
+
+router.get('/rejected', async (req, res) => {
+    try {
+        let viewable = await db.collection(rejectedCollection).find({}, { article: {_id: req, title: req}}).toArray();
+        
+        if (viewable.length !== 0) {// displays all articles
+            res.status(200).json(viewable); 
+        } else {
+            res.status(404).send("articles list data not found"); //aricles not found
+        }
+    } catch (error) {
+        console.error(error);
         res.status(500).send("Error connecting to database"); //cannot connect to database 
     }
 })
