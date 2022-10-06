c1202a436e9a191edd52dd185fdc9dc9cf61e167
MissChenduo <zdt5500@autuni.ac.nz>
2022-10-06 22:45:15 +1300

    moderateArticle
    Updated on moderateArticle

--- routes/moderate.js
+++ routes/moderate.js
@@ -24,30 +24,13 @@ router.get('/moderateArticles', async (req, res) => {
     }
 })
 
-router.get('/moderateArticles/:title', async (req, res) => {
-    try {
-        const {title} = req.params;
-
-            let viewable = await db.collection(submittedCollection).find({title
-                : title}).toArray();
-            
-            if (viewable.length !== 0) {// displays all articles
-                res.status(200).json(viewable); //pass
-            } else {
-                res.status(404).send("articles list data not found"); //aricles not found
-            }
-        } catch (error) {
-            console.error(error);
-        res.status(500).send("Error connecting to database"); //cannot connect to database 
-    }
-})
 
 router.get('/moderateArticles/accepted', async (req, res) => {
     try {
         let viewable = await db.collection(acceptedCollection).find({}, { article: {_id: req, title: req}}).toArray();
         
         if (viewable.length !== 0) {// displays all articles
-            res.status(200).json(viewable); 
+            res.status(200).json(moderate); 
         } else {
             res.status(404).send("articles list data not found"); //aricles not found
         }
@@ -62,7 +45,7 @@ router.get('/moderateArticles/rejected', async (req, res) => {
         let viewable = await db.collection(rejectedCollection).find({}, { article: {_id: req, title: req}}).toArray();
         
         if (viewable.length !== 0) {// displays all articles
-            res.status(200).json(viewable); 
+            res.status(200).json(moderate); 
         } else {
             res.status(404).send("articles list data not found"); //aricles not found
         }
